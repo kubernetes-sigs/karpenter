d805e84a371a5b2ac54b207a9ac0462c798417ab
Jonathan Innis <joinnis@amazon.com>
2022-10-25 15:11:24 -0500

    Remove mustParse call out of options.New()

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -75,7 +75,7 @@ func NewOperator() (context.Context, *Operator) {
 	ctx = sharedmain.WithHADisabled(ctx) // Disable leader election for webhook
 
 	// Options
-	opts := options.New()
+	opts := options.New().MustParse()
 	ctx = injection.WithOptions(ctx, *opts)
 
 	// Webhook
