131229a68d6cd6ad2527c479b5a6122ab4b9e1c5
Todd Neal <tnealt@amazon.com>
2022-12-08 12:41:46 -0600

    feat: add more deprovisioning logs (#109)

--- pkg/controllers/deprovisioning/drift.go
+++ pkg/controllers/deprovisioning/drift.go
@@ -64,7 +64,7 @@ func (d *Drift) ComputeCommand(ctx context.Context, candidates ...CandidateNode)
 	for _, candidate := range candidates {
 		// is this a node that we can terminate?  This check is meant to be fast so we can save the expense of simulated
 		// scheduling unless its really needed
-		if !canBeTerminated(candidate, pdbs) {
+		if _, canTerminate := canBeTerminated(candidate, pdbs); !canTerminate {
 			continue
 		}
 
