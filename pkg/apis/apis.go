deed22037972012864e48492d69842ac658a4054
Ellis Tarn <ellistarn@gmail.com>
2023-01-03 14:03:42 -0800

    fix: remove machine from webhook-gen (#149)

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -40,7 +40,6 @@ var (
 	// Resources defined in the project
 	Resources = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
 		v1alpha5.SchemeGroupVersion.WithKind("Provisioner"): &v1alpha5.Provisioner{},
-		v1alpha5.SchemeGroupVersion.WithKind("Machine"):     &v1alpha5.Machine{},
 	}
 	Settings = sets.New(settings.Registration)
 )
