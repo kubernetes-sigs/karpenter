6cc8e40e85da8f43ffdb189d8824b865380ee679
Lucas Sant' Anna <76971778+lucass4@users.noreply.github.com>
2023-01-30 18:24:28 -0500

    feat: add support for CPUCFSQuota

--- pkg/apis/v1alpha5/machine.go
+++ pkg/apis/v1alpha5/machine.go
@@ -101,6 +101,9 @@ type KubeletConfiguration struct {
 	// +kubebuilder:validation:Maximum:=100
 	// +optional
 	ImageGCLowThresholdPercent *int32 `json:"imageGCLowThresholdPercent,omitempty"`
+	// CpuCFSQuota enables CPU CFS quota enforcement for containers that specify CPU limits. Default: true
+	// +optional
+	CPUCFSQuota bool `json:"cpuCFSQuota,omitempty"`
 }
 
 type ProviderRef struct {
