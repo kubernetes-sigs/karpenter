269290631311359cbbfa185c639bb27f3f808cca
njtran <nichotr@amazon.com>
2023-10-26 12:18:34 -0700

    fix: do not terminate pods that tolerate unschedulable taint

--- pkg/controllers/node/termination/terminator/terminator.go
+++ pkg/controllers/node/termination/terminator/terminator.go
@@ -68,23 +68,25 @@ func (t *Terminator) Cordon(ctx context.Context, node *v1.Node) error {
 // https://kubernetes.io/docs/concepts/architecture/nodes/#graceful-node-shutdown
 func (t *Terminator) Drain(ctx context.Context, node *v1.Node) error {
 	// Get evictable pods
-	pods, err := t.getPods(ctx, node)
-	if err != nil {
-		return fmt.Errorf("listing pods for node, %w", err)
+	pods := &v1.PodList{}
+	if err := t.kubeClient.List(ctx, pods, client.MatchingFields{"spec.nodeName": node.Name}); err != nil {
+		return fmt.Errorf("listing pods on node, %w", err)
 	}
-	var podsToEvict []*v1.Pod
 	// Skip node due to pods that are not able to be evicted
-	for _, p := range pods {
-		// Ignore if unschedulable is tolerated, since they will reschedule
-		if podutil.ToleratesUnschedulableTaint(p) {
-			continue
+	podsToEvict := lo.FilterMap(pods.Items, func(po v1.Pod, _ int) (*v1.Pod, bool) {
+		p := lo.ToPtr(po)
+		// ignore pods that tolerate the noSchedule taint.
+		if podutil.ToleratesDisruptionNoScheduleTaint(p) ||
+			// Ignore static mirror pods
+			podutil.IsOwnedByNode(p) ||
+			// Ignore if the pod is complete and doesn't need to be evicted
+			podutil.IsTerminal(p) ||
+			// Ignore if kubelet is partitioned and pods are beyond graceful termination window
+			t.isStuckTerminating(p) {
+			return nil, false
 		}
-		// Ignore static mirror pods
-		if podutil.IsOwnedByNode(p) {
-			continue
-		}
-		podsToEvict = append(podsToEvict, p)
-	}
+		return p, true
+	})
 	// Enqueue for eviction
 	t.Evict(podsToEvict)
 
@@ -94,46 +96,41 @@ func (t *Terminator) Drain(ctx context.Context, node *v1.Node) error {
 	return nil
 }
 
-// getPods returns a list of evictable pods for the node
-func (t *Terminator) getPods(ctx context.Context, node *v1.Node) ([]*v1.Pod, error) {
-	podList := &v1.PodList{}
-	if err := t.kubeClient.List(ctx, podList, client.MatchingFields{"spec.nodeName": node.Name}); err != nil {
-		return nil, fmt.Errorf("listing pods on node, %w", err)
-	}
-	var pods []*v1.Pod
-	for _, p := range podList.Items {
-		// Ignore if the pod is complete and doesn't need to be evicted
-		if podutil.IsTerminal(lo.ToPtr(p)) {
-			continue
-		}
-		// Ignore if kubelet is partitioned and pods are beyond graceful termination window
-		if t.isStuckTerminating(lo.ToPtr(p)) {
-			continue
-		}
-		pods = append(pods, lo.ToPtr(p))
-	}
-	return pods, nil
-}
-
 func (t *Terminator) Evict(pods []*v1.Pod) {
-	// 1. Prioritize noncritical pods https://kubernetes.io/docs/concepts/architecture/nodes/#graceful-node-shutdown
-	critical := []*v1.Pod{}
-	nonCritical := []*v1.Pod{}
+	// 1. Prioritize noncritical pods, non-daemon pods https://kubernetes.io/docs/concepts/architecture/nodes/#graceful-node-shutdown
+	criticalNonDaemon := []*v1.Pod{}
+	criticalDaemon := []*v1.Pod{}
+	nonCriticalNonDaemon := []*v1.Pod{}
+	nonCriticalDaemon := []*v1.Pod{}
 	for _, pod := range pods {
 		if !pod.DeletionTimestamp.IsZero() {
 			continue
 		}
 		if pod.Spec.PriorityClassName == "system-cluster-critical" || pod.Spec.PriorityClassName == "system-node-critical" {
-			critical = append(critical, pod)
+			if podutil.IsOwnedByDaemonSet(pod) {
+				criticalDaemon = append(criticalDaemon, pod)
+			}
+			criticalNonDaemon = append(criticalNonDaemon, pod)
 		} else {
-			nonCritical = append(nonCritical, pod)
+			if podutil.IsOwnedByDaemonSet(pod) {
+				nonCriticalDaemon = append(nonCriticalDaemon, pod)
+			}
+			nonCriticalNonDaemon = append(nonCriticalNonDaemon, pod)
 		}
 	}
-	// 2. Evict critical pods if all noncritical are evicted
-	if len(nonCritical) == 0 {
-		t.evictionQueue.Add(critical...)
-	} else {
-		t.evictionQueue.Add(nonCritical...)
+	// 2. Evict in order:
+	// a. non-critical non-daemonsets
+	// b. non-critical daemonsets
+	// c. critical non-daemonsets
+	// d. critical daemonsets
+	if len(nonCriticalNonDaemon) != 0 {
+		t.evictionQueue.Add(nonCriticalNonDaemon...)
+	} else if len(nonCriticalDaemon) != 0 {
+		t.evictionQueue.Add(nonCriticalDaemon...)
+	} else if len(criticalNonDaemon) != 0 {
+		t.evictionQueue.Add(nonCriticalDaemon...)
+	} else if len(criticalDaemon) != 0 {
+		t.evictionQueue.Add(criticalDaemon...)
 	}
 }
 
