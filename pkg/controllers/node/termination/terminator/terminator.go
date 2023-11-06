b617ca4f2e9c1ff6244d5ac0fd101ece84b32d9e
njtran <nichotr@amazon.com>
2023-11-05 18:46:32 -0800

    chore: remove machine taint code

--- pkg/controllers/node/termination/terminator/terminator.go
+++ pkg/controllers/node/termination/terminator/terminator.go
@@ -45,19 +45,13 @@ func NewTerminator(clk clock.Clock, kubeClient client.Client, eq *Queue) *Termin
 	}
 }
 
-// Taint idempotently adds the karpenter.sh/disruption taint to a node with a NodeClaim
+// Taint adds the karpenter.sh/disruption taint to a node with a NodeClaim
 func (t *Terminator) Taint(ctx context.Context, node *v1.Node) error {
 	stored := node.DeepCopy()
-	// If the taint already has the karpenter.sh/disruption=disrupting:NoSchedule taint, do nothing.
-	if _, ok := lo.Find(node.Spec.Taints, func(t v1.Taint) bool {
+	node.Spec.Taints = lo.Reject(node.Spec.Taints, func(t v1.Taint, _ int) bool {
 		return v1beta1.IsDisruptingTaint(t)
-	}); !ok {
-		// If the taint key exists (but with a different value or effect), remove it.
-		node.Spec.Taints = lo.Reject(node.Spec.Taints, func(t v1.Taint, _ int) bool {
-			return t.Key == v1beta1.DisruptionTaintKey
-		})
-		node.Spec.Taints = append(node.Spec.Taints, v1beta1.DisruptionNoScheduleTaint)
-	}
+	})
+	node.Spec.Taints = append(node.Spec.Taints, v1beta1.DisruptionNoScheduleTaint)
 	// Adding this label to the node ensures that the node is removed from the load-balancer target group
 	// while it is draining and before it is terminated. This prevents 500s coming prior to health check
 	// when the load balancer controller hasn't yet determined that the node and underlying connections are gone
