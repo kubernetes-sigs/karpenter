285d3800988368da14439a4e9429656edb0f1bae
nikmohan <nikmohan@amazon.com>
2024-03-05 20:24:57 -0600

    Changing the MaxInstanceTypes back to original value of 60 inorder to reduce the request size to be sent to launch APIs.

--- pkg/controllers/provisioning/scheduling/nodeclaimtemplate.go
+++ pkg/controllers/provisioning/scheduling/nodeclaimtemplate.go
@@ -31,7 +31,7 @@ import (
 
 // MaxInstanceTypes is a constant that restricts the number of instance types to be sent for launch. Note that this
 // is intentionally changed to var just to help in testing the code.
-var MaxInstanceTypes = 100
+var MaxInstanceTypes = 60
 
 // NodeClaimTemplate encapsulates the fields required to create a node and mirrors
 // the fields in NodePool. These structs are maintained separately in order
