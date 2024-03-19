17874fe92c4dec1ce133a0b8c6b7a5fbe46b2cbd
Bryce Soghigian <bsoghigian@gmail.com>
2024-03-19 08:44:32 +0100

    fix: fixing consolidation test to use new empty reason

--- pkg/controllers/disruption/suite_test.go
+++ pkg/controllers/disruption/suite_test.go
@@ -645,7 +645,7 @@ var _ = Describe("BuildDisruptionBudgetMapping", func() {
 		budgets, err := disruption.BuildDisruptionBudgets(ctx, cluster, fakeClock, env.Client, recorder)
 		Expect(err).To(Succeed())
 		// This should not bring in the unmanaged node.
-		for _, reason := range v1beta1.DisruptionReasons {
+		for _, reason := range v1beta1.WellKnownDisruptionReasons {
 			Expect(budgets[nodePool.Name][reason]).To(Equal(10))
 		}
 	})
@@ -676,7 +676,7 @@ var _ = Describe("BuildDisruptionBudgetMapping", func() {
 		budgets, err := disruption.BuildDisruptionBudgets(ctx, cluster, fakeClock, env.Client, recorder)
 		Expect(err).To(Succeed())
 		// This should not bring in the uninitialized node.
-		for _, reason := range v1beta1.DisruptionReasons {
+		for _, reason := range v1beta1.WellKnownDisruptionReasons {
 			Expect(budgets[nodePool.Name][reason]).To(Equal(10))
 		}
 	})
@@ -697,7 +697,7 @@ var _ = Describe("BuildDisruptionBudgetMapping", func() {
 
 		budgets, err := disruption.BuildDisruptionBudgets(ctx, cluster, fakeClock, env.Client, recorder)
 		Expect(err).To(Succeed())
-		for _, reason := range v1beta1.DisruptionReasons {
+		for _, reason := range v1beta1.WellKnownDisruptionReasons {
 			Expect(budgets[nodePool.Name][reason]).To(Equal(0))
 		}
 	})
@@ -722,7 +722,7 @@ var _ = Describe("BuildDisruptionBudgetMapping", func() {
 		budgets, err := disruption.BuildDisruptionBudgets(ctx, cluster, fakeClock, env.Client, recorder)
 		Expect(err).To(Succeed())
 
-		for _, reason := range v1beta1.DisruptionReasons {
+		for _, reason := range v1beta1.WellKnownDisruptionReasons {
 			Expect(budgets[nodePool.Name][reason]).To(Equal(8))
 		}
 	})
@@ -743,7 +743,7 @@ var _ = Describe("BuildDisruptionBudgetMapping", func() {
 
 		budgets, err := disruption.BuildDisruptionBudgets(ctx, cluster, fakeClock, env.Client, recorder)
 		Expect(err).To(Succeed())
-		for _, reason := range v1beta1.DisruptionReasons {
+		for _, reason := range v1beta1.WellKnownDisruptionReasons {
 			Expect(budgets[nodePool.Name][reason]).To(Equal(8))
 		}
 	})
