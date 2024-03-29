bcd05de963001de9f3a29e4151d52c5e218a4b15
Amanuel Engeda <aengeda@amazon.com>
2024-03-29 10:01:32 -0700

    Get the supported nodeclass from the cloudprovider

--- pkg/cloudprovider/types.go
+++ pkg/cloudprovider/types.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/samber/lo"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	"sigs.k8s.io/karpenter/pkg/apis/v1beta1"
 	"sigs.k8s.io/karpenter/pkg/scheduling"
@@ -55,6 +56,8 @@ type CloudProvider interface {
 	IsDrifted(context.Context, *v1beta1.NodeClaim) (DriftReason, error)
 	// Name returns the CloudProvider implementation name.
 	Name() string
+	// GetSupportedNodeClass returns the group, version, and kind of the CloudProvider NodeClass
+	GetSupportedNodeClasses() []schema.GroupVersionKind
 }
 
 // InstanceType describes the properties of a potential node (either concrete attributes of an instance of this type
