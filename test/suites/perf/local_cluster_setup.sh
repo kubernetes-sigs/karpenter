b39d1aa85fc9fc22a2caa13641c728516dac35b5
Nathaniel Jones <jonesflp@amazon.com>
2024-06-20 15:05:30 -0700

    Almost have the testing environment built to be able to finish the sim testing

--- test/suites/perf/local_cluster_setup.sh
+++ test/suites/perf/local_cluster_setup.sh
@@ -1,36 +1,11 @@
-context_name="karpenter-test-kind-cluster"
+context_name="chart-testing"
 prom_ns="prometheus"
+
+
 kind create cluster --name ${context_name}
 cd ..
 cd ..
 cd ..
-make apply-with-kind
-cat <<EOF | envsubst | kubectl apply -f -
-apiVersion: karpenter.sh/v1beta1
-kind: NodePool
-metadata:
-  name: default
-spec:
-  template:
-    spec:
-      requirements:
-        - key: kubernetes.io/arch
-          operator: In
-          values: ["amd64"]
-        - key: kubernetes.io/os
-          operator: In
-          values: ["linux"]
-        - key: karpenter.sh/capacity-type
-          operator: In
-          values: ["spot"]
-      nodeClassRef:
-        name: nil
-  limits:
-    cpu: 1500
-  disruption:
-    consolidationPolicy: WhenUnderutilized
-    expireAfter: 720h # 30 * 24h = 720h
-EOF
 kubectl create namespace "$prom_ns"
 helm upgrade --values valuesv3.yaml --install prometheus prometheus-community/kube-prometheus-stack -n "$prom_ns" \
   --set "kubelet.serviceMonitor.cAdvisorRelabelings[0].targetLabel=metrics_path" \
@@ -45,12 +20,49 @@ helm upgrade --values valuesv3.yaml --install prometheus prometheus-community/ku
   --set "kubelet.serviceMonitor.cAdvisorRelabelings[4].targetLabel=commitsAfterTag" \
   --set "kubelet.serviceMonitor.cAdvisorRelabelings[4].replacement=\"$(git describe --tags | cut -d '-' -f 2)\"" \
   --wait
+#make toolchain
+make install-kwok
+KWOK_REPO=kind.local
+KIND_CLUSTER_NAME=chart-testing
+make apply-with-kind
+
 # Testing out pyroscope for profiling
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 # This only creates one replica for testing, but not multiple backends
 #kubectl create namespace pyroscope-test
-helm -n karpenter install pyroscope grafana/pyroscope
-make test
+helm -n kube-system install pyroscope grafana/pyroscope
+kubectl taint nodes chart-testing-control-plane CriticalAddonsOnly:NoSchedule
+#cat <<EOF | envsubst | kubectl apply -f -
+#apiVersion: karpenter.sh/v1beta1
+#kind: NodePool
+#metadata:
+#  name: default
+#spec:
+#  template:
+#    spec:
+#      requirements:
+#        - key: kubernetes.io/arch
+#          operator: In
+#          values: ["amd64"]
+#        - key: kubernetes.io/os
+#          operator: In
+#          values: ["linux"]
+#        - key: karpenter.sh/capacity-type
+#          operator: In
+#          values: ["spot"]
+#      nodeClassRef:
+#        name: nil
+#  limits:
+#    cpu: 1500
+#  disruption:
+#    consolidationPolicy: WhenUnderutilized
+#    expireAfter: 720h # 30 * 24h = 720h
+#EOF
+kubectl get pods -n kube-system | grep karpenter
+kubectl get nodepools
+kubectl get pods -A
+kubectl describe nodes
+make e2etests
 read -p "press enter to delete cluster" temp_var
 kind delete cluster --name ${context_name}
