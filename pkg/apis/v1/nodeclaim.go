0011ad2956efb15f2cee845e1fbe88387bc243a2
Amanuel Engeda <74629455+engedaam@users.noreply.github.com>
2024-06-28 13:51:47 -0700

    chore: Remove kubelet configuration from the v1 APIs (#1358)

--- pkg/apis/v1/nodeclaim.go
+++ pkg/apis/v1/nodeclaim.go
@@ -43,13 +43,6 @@ type NodeClaimSpec struct {
 	// Resources models the resource requirements for the NodeClaim to launch
 	// +optional
 	Resources ResourceRequirements `json:"resources,omitempty" hash:"ignore"`
-	// Kubelet defines args to be used when configuring kubelet on provisioned nodes.
-	// They are a subset of the upstream types, recognizing not all options may be supported.
-	// Wherever possible, the types and names should reflect the upstream kubelet types.
-	// +kubebuilder:validation:XValidation:message="imageGCHighThresholdPercent must be greater than imageGCLowThresholdPercent",rule="has(self.imageGCHighThresholdPercent) && has(self.imageGCLowThresholdPercent) ?  self.imageGCHighThresholdPercent > self.imageGCLowThresholdPercent  : true"
-	// +kubebuilder:validation:XValidation:message="evictionSoft OwnerKey does not have a matching evictionSoftGracePeriod",rule="has(self.evictionSoft) ? self.evictionSoft.all(e, (e in self.evictionSoftGracePeriod)):true"
-	// +kubebuilder:validation:XValidation:message="evictionSoftGracePeriod OwnerKey does not have a matching evictionSoft",rule="has(self.evictionSoftGracePeriod) ? self.evictionSoftGracePeriod.all(e, (e in self.evictionSoft)):true"
-	Kubelet *KubeletConfiguration `json:"kubelet,omitempty"`
 	// NodeClassRef is a reference to an object that defines provider specific configuration
 	// +required
 	NodeClassRef *NodeClassReference `json:"nodeClassRef"`
@@ -73,75 +66,6 @@ type ResourceRequirements struct {
 	// Requests describes the minimum required resources for the NodeClaim to launch
 	// +optional
 	Requests v1.ResourceList `json:"requests,omitempty"`
-}
-
-// KubeletConfiguration defines args to be used when configuring kubelet on provisioned nodes.
-// They are a subset of the upstream types, recognizing not all options may be supported.
-// Wherever possible, the types and names should reflect the upstream kubelet types.
-// https://pkg.go.dev/k8s.io/kubelet/config/v1beta1#KubeletConfiguration
-// https://github.com/kubernetes/kubernetes/blob/9f82d81e55cafdedab619ea25cabf5d42736dacf/cmd/kubelet/app/options/options.go#L53
-type KubeletConfiguration struct {
-	// clusterDNS is a list of IP addresses for the cluster DNS server.
-	// Note that not all providers may use all addresses.
-	//+optional
-	ClusterDNS []string `json:"clusterDNS,omitempty"`
-	// MaxPods is an override for the maximum number of pods that can run on
-	// a worker node instance.
-	// +kubebuilder:validation:Minimum:=0
-	// +optional
-	MaxPods *int32 `json:"maxPods,omitempty"`
-	// PodsPerCore is an override for the number of pods that can run on a worker node
-	// instance based on the number of cpu cores. This value cannot exceed MaxPods, so, if
-	// MaxPods is a lower value, that value will be used.
-	// +kubebuilder:validation:Minimum:=0
-	// +optional
-	PodsPerCore *int32 `json:"podsPerCore,omitempty"`
-	// SystemReserved contains resources reserved for OS system daemons and kernel memory.
-	// +kubebuilder:validation:XValidation:message="valid keys for systemReserved are ['cpu','memory','ephemeral-storage','pid']",rule="self.all(x, x=='cpu' || x=='memory' || x=='ephemeral-storage' || x=='pid')"
-	// +kubebuilder:validation:XValidation:message="systemReserved value cannot be a negative resource quantity",rule="self.all(x, !self[x].startsWith('-'))"
-	// +optional
-	SystemReserved map[string]string `json:"systemReserved,omitempty"`
-	// KubeReserved contains resources reserved for Kubernetes system components.
-	// +kubebuilder:validation:XValidation:message="valid keys for kubeReserved are ['cpu','memory','ephemeral-storage','pid']",rule="self.all(x, x=='cpu' || x=='memory' || x=='ephemeral-storage' || x=='pid')"
-	// +kubebuilder:validation:XValidation:message="kubeReserved value cannot be a negative resource quantity",rule="self.all(x, !self[x].startsWith('-'))"
-	// +optional
-	KubeReserved map[string]string `json:"kubeReserved,omitempty"`
-	// EvictionHard is the map of signal names to quantities that define hard eviction thresholds
-	// +kubebuilder:validation:XValidation:message="valid keys for evictionHard are ['memory.available','nodefs.available','nodefs.inodesFree','imagefs.available','imagefs.inodesFree','pid.available']",rule="self.all(x, x in ['memory.available','nodefs.available','nodefs.inodesFree','imagefs.available','imagefs.inodesFree','pid.available'])"
-	// +optional
-	EvictionHard map[string]string `json:"evictionHard,omitempty"`
-	// EvictionSoft is the map of signal names to quantities that define soft eviction thresholds
-	// +kubebuilder:validation:XValidation:message="valid keys for evictionSoft are ['memory.available','nodefs.available','nodefs.inodesFree','imagefs.available','imagefs.inodesFree','pid.available']",rule="self.all(x, x in ['memory.available','nodefs.available','nodefs.inodesFree','imagefs.available','imagefs.inodesFree','pid.available'])"
-	// +optional
-	EvictionSoft map[string]string `json:"evictionSoft,omitempty"`
-	// EvictionSoftGracePeriod is the map of signal names to quantities that define grace periods for each eviction signal
-	// +kubebuilder:validation:XValidation:message="valid keys for evictionSoftGracePeriod are ['memory.available','nodefs.available','nodefs.inodesFree','imagefs.available','imagefs.inodesFree','pid.available']",rule="self.all(x, x in ['memory.available','nodefs.available','nodefs.inodesFree','imagefs.available','imagefs.inodesFree','pid.available'])"
-	// +optional
-	EvictionSoftGracePeriod map[string]metav1.Duration `json:"evictionSoftGracePeriod,omitempty"`
-	// EvictionMaxPodGracePeriod is the maximum allowed grace period (in seconds) to use when terminating pods in
-	// response to soft eviction thresholds being met.
-	// +optional
-	EvictionMaxPodGracePeriod *int32 `json:"evictionMaxPodGracePeriod,omitempty"`
-	// ImageGCHighThresholdPercent is the percent of disk usage after which image
-	// garbage collection is always run. The percent is calculated by dividing this
-	// field value by 100, so this field must be between 0 and 100, inclusive.
-	// When specified, the value must be greater than ImageGCLowThresholdPercent.
-	// +kubebuilder:validation:Minimum:=0
-	// +kubebuilder:validation:Maximum:=100
-	// +optional
-	ImageGCHighThresholdPercent *int32 `json:"imageGCHighThresholdPercent,omitempty"`
-	// ImageGCLowThresholdPercent is the percent of disk usage before which image
-	// garbage collection is never run. Lowest disk usage to garbage collect to.
-	// The percent is calculated by dividing this field value by 100,
-	// so the field value must be between 0 and 100, inclusive.
-	// When specified, the value must be less than imageGCHighThresholdPercent
-	// +kubebuilder:validation:Minimum:=0
-	// +kubebuilder:validation:Maximum:=100
-	// +optional
-	ImageGCLowThresholdPercent *int32 `json:"imageGCLowThresholdPercent,omitempty"`
-	// CPUCFSQuota enables CPU CFS quota enforcement for containers that specify CPU limits.
-	// +optional
-	CPUCFSQuota *bool `json:"cpuCFSQuota,omitempty"`
 }
 
 type NodeClassReference struct {
