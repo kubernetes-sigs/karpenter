df7682a4213e17a40fea28d2a90b9ccdac2ccadf
daimaxiaxie <codexiaxie@163.com>
2024-06-30 16:08:02 +0800

    disableCaller from LogLevel

--- pkg/operator/logging/logging.go
+++ pkg/operator/logging/logging.go
@@ -51,10 +51,14 @@ func DefaultZapConfig(ctx context.Context, component string) zap.Config {
 		// Webhooks are deprecated, so support for changing their log level is also deprecated
 		logLevel = lo.Must(zap.ParseAtomicLevel(l))
 	}
+	disableCaller := true
+	if l := options.FromContext(ctx).LogLevel; l == "debug" {
+		disableCaller = false
+	}
 	return zap.Config{
 		Level:             logLevel,
 		Development:       false,
-		DisableCaller:     true,
+		DisableCaller:     disableCaller,
 		DisableStacktrace: true,
 		Sampling: &zap.SamplingConfig{
 			Initial:    100,
