cf5df0e1765c711d27a86098d9973913e72368f4
Amanuel Engeda <aengeda@amazon.com>
2024-07-31 03:55:59 -0700

    Add Conversion Webhooks

---
+++ pkg/apis/v1beta1/nodeclaim_conversion.go
@@ -0,0 +1,29 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"context"
+
+	"knative.dev/pkg/apis"
+)
+
+// Since v1 is the hub conversion version, We will only need to implement conversion webhooks for v1
+
+func (in *NodeClaim) ConvertTo(_ context.Context, _ apis.Convertible) error { return nil }
+
+func (in *NodeClaim) ConvertFrom(_ context.Context, _ apis.Convertible) error { return nil }
