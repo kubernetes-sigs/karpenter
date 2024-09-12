3038ff6cbf643bd0e0f54fb05d827ac0da471030
Nick Tran <nichotr@amazon.com>
2024-09-12 11:27:48 -0700

    fix: add default disruption stanza

--- pkg/apis/v1/nodepool.go
+++ pkg/apis/v1/nodepool.go
@@ -42,6 +42,7 @@ type NodePoolSpec struct {
 	// +required
 	Template NodeClaimTemplate `json:"template"`
 	// Disruption contains the parameters that relate to Karpenter's disruption logic
+	// +kubebuilder:default:={consolidationPolicy: "WhenEmptyOrUnderutilized", consolidateAfter: "0s"}
 	// +optional
 	Disruption Disruption `json:"disruption"`
 	// Limits define a set of bounds for provisioning capacity.
