1c22eda5569833b92445365923fff60bf98a9aad
Reed Schalo <schalor@amazon.com>
2024-10-23 14:40:52 -0700

    fix: check for nil pointer dereference (#1763)

--- pkg/controllers/disruption/multinodeconsolidation.go
+++ pkg/controllers/disruption/multinodeconsolidation.go
@@ -209,7 +209,10 @@ func filterOutSameType(newNodeClaim *scheduling.NodeClaim, consolidate []*Candid
 	}
 	// swallow the error since we don't allow min values to impact reschedulability in multi node claim
 	newNodeClaim, err := newNodeClaim.RemoveInstanceTypeOptionsByPriceAndMinValues(newNodeClaim.Requirements, maxPrice)
-	return newNodeClaim.InstanceTypeOptions, err
+	if err != nil {
+		return nil, err
+	}
+	return newNodeClaim.InstanceTypeOptions, nil
 }
 
 func (m *MultiNodeConsolidation) Reason() v1.DisruptionReason {
