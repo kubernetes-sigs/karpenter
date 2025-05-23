cb7a0913b853bb9695376afded988eb00eb20955
Reed Schalo <schalor@amazon.com>
2025-05-22 17:39:13 -0700

    update sleep

--- test/suites/regression/nodeclaim_test.go
+++ test/suites/regression/nodeclaim_test.go
@@ -248,7 +248,6 @@ var _ = Describe("NodeClaim", func() {
 		})
 		It("should delete a NodeClaim if it references a NodeClass that isn't Ready", func() {
 			nodeClass = env.InvalidNodeClass.DeepCopy()
-			time.Sleep(10 * time.Second)
 			env.ExpectCreated(nodeClass)
 			nodeClass = env.ExpectNodeClassCondition(nodeClass, []status.Condition{
 				{
