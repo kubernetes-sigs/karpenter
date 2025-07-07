2d510f3d72ebc4d7593f96e4923f8329ed220a9d
Garvin Pang <garvinp@stripe.com>
2025-07-07 10:08:47 -0700

    add default just to nodepool

--- pkg/apis/v1/nodepool.go
+++ pkg/apis/v1/nodepool.go
@@ -218,6 +218,7 @@ type NodeClaimTemplateSpec struct {
 	// If left undefined, the controller will use the default 15m for the node to register.
 	// +kubebuilder:validation:Pattern=`^([0-9]+(s|m|h))+$`
 	// +kubebuilder:validation:Type="string"
+	// +kubebuilder:default:="15m"
 	// +optional
 	RegistrationTTL *metav1.Duration `json:"registrationTTL,omitempty" hash:"ignore"`
 }
