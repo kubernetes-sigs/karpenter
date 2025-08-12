11e7784958df23c54ba2668a0fe54dd7c7eb5583
DerekFrank <dfrankmn@protonmail.com>
2025-08-12 16:27:25 -0700

    feat: Create optional operator arguments to leverage leader lease functionality

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -28,6 +28,7 @@ import (
 
 	"github.com/awslabs/operatorpkg/controller"
 	opmetrics "github.com/awslabs/operatorpkg/metrics"
+	"github.com/awslabs/operatorpkg/option"
 	"github.com/awslabs/operatorpkg/serrors"
 	"github.com/go-logr/zapr"
 	"github.com/prometheus/client_golang/prometheus"
@@ -102,8 +103,14 @@ type Operator struct {
 	Clock               clock.Clock
 }
 
+type OperatorOpts struct {
+	LeaderElectionLabels map[string]string
+}
+
 // NewOperator instantiates a controller manager or panics
-func NewOperator() (context.Context, *Operator) {
+func NewOperator(o ...option.Function[OperatorOpts]) (context.Context, *Operator) {
+	opts := option.Resolve(o...)
+
 	// Root Context
 	ctx := context.Background()
 
@@ -149,6 +156,7 @@ func NewOperator() (context.Context, *Operator) {
 		LeaderElectionResourceLock:    resourcelock.LeasesResourceLock,
 		LeaderElectionReleaseOnCancel: true,
 		LeaderElectionConfig:          leaderConfig,
+		LeaderElectionLabels:          opts.LeaderElectionLabels,
 		Metrics: server.Options{
 			BindAddress: fmt.Sprintf(":%d", options.FromContext(ctx).MetricsPort),
 		},
