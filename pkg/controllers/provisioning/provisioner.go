021a33c18225dbdb1aebb1fa2dd8f19aac9abb02
rsumukha <rsumukha@amazon.com>
2025-08-25 09:27:24 -0700

    Fix Build

--- pkg/controllers/provisioning/provisioner.go
+++ pkg/controllers/provisioning/provisioner.go
@@ -42,7 +42,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"sigs.k8s.io/karpenter/pkg/operator/options"
 
@@ -144,7 +143,7 @@ func (p *Provisioner) Reconcile(ctx context.Context) (result reconciler.Result, 
 		return reconciler.Result{RequeueAfter: singleton.RequeueImmediately}, nil
 	}
 	if _, err = p.CreateNodeClaims(ctx, results.NewNodeClaims, WithReason(metrics.ProvisionedReason), RecordPodNomination, IgnoreStaticNodeClaims); err != nil {
-		return reconcile.Result{}, err
+		return reconciler.Result{}, err
 	}
 	return reconciler.Result{RequeueAfter: singleton.RequeueImmediately}, nil
 }
