0318d67a3c332ecbff0e2798caa65520462744c7
Max Cao <macao@redhat.com>
2025-09-19 17:05:53 -0700

    feat(limits): support node limits
    Nodepool node limits are now enforced during provisioning, aligning it with other resource limits.
    This commit adds the Node resource as a static field to the capacity of a StateNode which will always equal 1.
    
    Signed-off-by: Max Cao <macao@redhat.com>

--- pkg/controllers/provisioning/scheduling/scheduler.go
+++ pkg/controllers/provisioning/scheduling/scheduler.go
@@ -603,6 +603,12 @@ func (s *Scheduler) addToNewNodeClaim(ctx context.Context, pod *corev1.Pod) erro
 		its := s.nodeClaimTemplates[i].InstanceTypeOptions
 		// if limits have been applied to the nodepool, ensure we filter instance types to avoid violating those limits
 		if remaining, ok := s.remainingResources[s.nodeClaimTemplates[i].NodePoolName]; ok {
+			// Node limits can be enforced early, since we know exactly how much capacity in nodes will be consumed by any instance type (1 node).
+			nodesRemaining, ok := remaining[resources.Node]
+			if ok && nodesRemaining.IsZero() {
+				errs[i] = serrors.Wrap(fmt.Errorf("node limits have been exhausted for nodepool"), "NodePool", klog.KRef("", s.nodeClaimTemplates[i].NodePoolName))
+				return true
+			}
 			its = filterByRemainingResources(its, remaining)
 			if len(its) == 0 {
 				errs[i] = serrors.Wrap(fmt.Errorf("all available instance types exceed limits for nodepool"), "NodePool", klog.KRef("", s.nodeClaimTemplates[i].NodePoolName))
