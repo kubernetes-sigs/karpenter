d22b6027240071b238f1e8c65b5ea659df3183ac
Nathaniel Jones <jonesflp@amazon.com>
2025-10-29 13:30:31 -0700

    Trying 1000

--- test/suites/performance/large_scale_test.go
+++ test/suites/performance/large_scale_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Performance", func() {
 
 			// Create deployment with small resource requirements
 			smallDeployment := test.Deployment(test.DeploymentOptions{
-				Replicas: int32(400),
+				Replicas: int32(500),
 				PodOptions: test.PodOptions{
 					ObjectMeta: metav1.ObjectMeta{
 						Labels: map[string]string{
@@ -108,7 +108,7 @@ var _ = Describe("Performance", func() {
 
 			// Create deployment with large resource requirements
 			largeDeployment := test.Deployment(test.DeploymentOptions{
-				Replicas: int32(400),
+				Replicas: int32(500),
 				PodOptions: test.PodOptions{
 					ObjectMeta: metav1.ObjectMeta{
 						Labels: map[string]string{
@@ -141,7 +141,7 @@ var _ = Describe("Performance", func() {
 
 			// Wait for all pods to become healthy with a 15-minute timeout
 			// This covers both scheduling and readiness
-			env.EventuallyExpectHealthyPodCountWithTimeout(15*time.Minute, allPodsSelector, 800)
+			env.EventuallyExpectHealthyPodCountWithTimeout(15*time.Minute, allPodsSelector, 1000)
 
 			env.TimeIntervalCollector.End("waiting_for_pods")
 			env.TimeIntervalCollector.End("test_start")
@@ -303,8 +303,8 @@ var _ = Describe("Performance", func() {
 				"Average memory utilization should be greater than 70%")
 
 			// Verify all pods are actually running
-			env.EventuallyExpectHealthyPodCount(smallPodSelector, 400)
-			env.EventuallyExpectHealthyPodCount(largePodSelector, 400)
+			env.EventuallyExpectHealthyPodCount(smallPodSelector, 500)
+			env.EventuallyExpectHealthyPodCount(largePodSelector, 500)
 
 			By("Performance test completed successfully")
 		})
