a1357ee7fbf3fcc814c89e06a73df21c36453dae
Derek Frank <dfrankmn@protonmail.com>
2025-11-13 11:07:38 -0800

    feat: add a nodepool cost metric (#2584)

--- pkg/controllers/static/provisioning/suite_test.go
+++ pkg/controllers/static/provisioning/suite_test.go
@@ -43,6 +43,7 @@ import (
 	static "sigs.k8s.io/karpenter/pkg/controllers/static/provisioning"
 	"sigs.k8s.io/karpenter/pkg/events"
 	"sigs.k8s.io/karpenter/pkg/operator/options"
+	"sigs.k8s.io/karpenter/pkg/state/cost"
 	"sigs.k8s.io/karpenter/pkg/test"
 	. "sigs.k8s.io/karpenter/pkg/test/expectations"
 	"sigs.k8s.io/karpenter/pkg/test/v1alpha1"
@@ -71,6 +72,7 @@ var (
 	env                      *test.Environment
 	nodeClaimStateController *informer.NodeClaimController
 	prov                     *provisioning.Provisioner
+	clusterCost              *cost.ClusterCost
 )
 
 func TestAPIs(t *testing.T) {
@@ -85,11 +87,12 @@ var _ = BeforeSuite(func() {
 	cloudProvider = fake.NewCloudProvider()
 	prov = provisioning.NewProvisioner(env.Client, events.NewRecorder(&record.FakeRecorder{}), cloudProvider, cluster, fakeClock)
 	fakeClock = clock.NewFakeClock(time.Now())
+	clusterCost = cost.NewClusterCost(ctx, cloudProvider, env.Client)
 	cluster = state.NewCluster(fakeClock, env.Client, cloudProvider)
 	nodeController = informer.NewNodeController(env.Client, cluster)
 	daemonsetController = informer.NewDaemonSetController(env.Client, cluster)
 	controller = static.NewController(env.Client, cluster, events.NewRecorder(&record.FakeRecorder{}), cloudProvider, prov, fakeClock)
-	nodeClaimStateController = informer.NewNodeClaimController(env.Client, cloudProvider, cluster)
+	nodeClaimStateController = informer.NewNodeClaimController(env.Client, cloudProvider, cluster, clusterCost)
 })
 
 var _ = BeforeEach(func() {
