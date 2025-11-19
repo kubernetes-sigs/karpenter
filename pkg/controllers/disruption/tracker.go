82a092e963933e6a610072d75ef877a6ab50bb7a
DerekFrank <dfrankmn@protonmail.com>
2025-11-19 10:38:47 -0800

    Adding tracker_test, and switch to pods for source of desired state

--- pkg/controllers/disruption/tracker.go
+++ pkg/controllers/disruption/tracker.go
@@ -23,19 +23,18 @@ import (
 	"sort"
 	"sync"
 
+	corev1 "k8s.io/api/core/v1"
+
 	cache "github.com/patrickmn/go-cache"
 	"github.com/samber/lo"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/utils/clock"
 
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	v1 "sigs.k8s.io/karpenter/pkg/apis/v1"
 	"sigs.k8s.io/karpenter/pkg/controllers/state"
 	"sigs.k8s.io/karpenter/pkg/state/cost"
-	"sigs.k8s.io/karpenter/pkg/utils/resources"
+	"sigs.k8s.io/karpenter/pkg/state/podresources"
 )
 
 type Tracker struct {
@@ -45,26 +44,25 @@ type Tracker struct {
 	bucketsThresholdMap map[string]*BucketThresholds
 	enabled             bool
 
-	kubeClient  client.Client
-	cluster     *state.Cluster
-	clusterCost *cost.ClusterCost
-	clock       clock.Clock
+	cluster      *state.Cluster
+	clusterCost  *cost.ClusterCost
+	podResources *podresources.PodResources
+	clock        clock.Clock
 }
 
 // NewTracker creates a queue that will asynchronously track and emit metrics on decisions
-func NewTracker(cluster *state.Cluster, clock clock.Clock, decisionCache *cache.Cache, bucketsThresholdMap map[string]*BucketThresholds, enabled bool) *Tracker {
-	decisionCache.OnEvicted(DecisionExpired)
+func NewTracker(cluster *state.Cluster, clusterCost *cost.ClusterCost, podResources *podresources.PodResources, clock clock.Clock, decisionCache *cache.Cache, bucketsThresholdMap map[string]*BucketThresholds, enabled bool) *Tracker {
 	bucketsThresholdMapCopy := map[string]*BucketThresholds{}
 	if bucketsThresholdMap != nil {
 		maps.Copy(bucketsThresholdMapCopy, bucketsThresholdMap)
 		for _, bt := range bucketsThresholdMapCopy {
-			sort.Slice(bt.thresholds, func(i, j int) bool {
-				return bt.thresholds[i].lowerBound < bt.thresholds[j].lowerBound
+			sort.Slice(bt.Thresholds, func(i, j int) bool {
+				return bt.Thresholds[i].LowerBound < bt.Thresholds[j].LowerBound
 			})
 		}
-		for _, b := range DecisionDimensions {
+		for _, b := range DecisionBucketDimensions {
 			_, exists := bucketsThresholdMapCopy[b]
-			if !exists {
+			if !exists && enabled {
 				panic(fmt.Errorf("missing thresholds for necessary bucket %s", b))
 			}
 		}
@@ -76,46 +74,20 @@ func NewTracker(cluster *state.Cluster, clock clock.Clock, decisionCache *cache.
 		bucketsThresholdMap: bucketsThresholdMapCopy,
 		enabled:             enabled,
 		cluster:             cluster,
+		clusterCost:         clusterCost,
+		podResources:        podResources,
 		clock:               clock,
 	}
 	return tracker
 }
 
 func (t *Tracker) GatherClusterState(ctx context.Context) (*ClusterStateSnapshot, error) {
-	deployments := appsv1.DeploymentList{}
-	err := t.kubeClient.List(ctx, &deployments)
-	if err != nil {
-		return nil, err
-	}
-
-	totalDesiredPodCount := 0
-	totalDesiredPodCount = lo.SumBy(deployments.Items, func(d appsv1.Deployment) int {
-		return int(*d.Spec.Replicas)
-	})
-
-	rs := lo.Map(deployments.Items, func(d appsv1.Deployment, _ int) corev1.ResourceList {
-		requests := lo.Map(d.Spec.Template.Spec.Containers, func(c corev1.Container, _ int) corev1.ResourceList {
-			return c.Resources.Requests
-		})
-
-		totalContainerResources := resources.Merge(requests...)
-
-		for i, r := range totalContainerResources {
-			r.Set(r.Value() * int64(*d.Spec.Replicas))
-			totalContainerResources[i] = r
-		}
-
-		return totalContainerResources
-	})
-
-	totalDesiredPodResources := resources.Merge(rs...)
-
 	return &ClusterStateSnapshot{
-		clusterCost:              t.clusterCost.GetClusterCost(),
-		podResources:             t.cluster.GetTotalPodResourceRequests(),
-		totalNodes:               len(t.cluster.DeepCopyNodes()),
-		totalDesiredPodCount:     totalDesiredPodCount,
-		totalDesiredPodResources: &totalDesiredPodResources,
+		ClusterCost:              t.clusterCost.GetClusterCost(),
+		PodResources:             t.cluster.GetTotalPodResourceRequests(),
+		TotalNodes:               len(t.cluster.DeepCopyNodes()),
+		TotalDesiredPodCount:     t.podResources.GetTotalPodCount(),
+		TotalDesiredPodResources: t.podResources.GetTotalPodResourceRequests(),
 	}, nil
 }
 
@@ -167,14 +139,15 @@ func (t *Tracker) FinishCommand(ctx context.Context, nc *v1.NodeClaim) {
 
 	d, exists := t.decisionCache.Get(nc.Name)
 	if !exists {
-		log.FromContext(ctx).Error(fmt.Errorf("decision not found in cache for NodeClaim"), "nc", nc)
+		log.FromContext(ctx).Error(fmt.Errorf("decision not found in cache for NodeClaim"), "finishing decision", "nc", nc)
 		DecisionTrackerErrors.Inc(map[string]string{})
 		t.Unlock()
 		return
 	}
-	decision, ok := d.(*TrackedDecision)
+	t.decisionCache.Delete(nc.Name)
+	decision, ok := d.(TrackedDecision)
 	if !ok {
-		log.FromContext(ctx).Error(fmt.Errorf("failed to cast cached item to Decision for NodeClaim"), "nc", nc)
+		log.FromContext(ctx).Error(fmt.Errorf("failed to cast cached item to Decision for NodeClaim"), "finishing decision", "nc", nc)
 		DecisionTrackerErrors.Inc(map[string]string{})
 		t.Unlock()
 		return
@@ -189,18 +162,7 @@ func (t *Tracker) FinishCommand(ctx context.Context, nc *v1.NodeClaim) {
 	}
 	// we know the decision is over, lets emit metrics
 	t.Unlock()
-	t.EmitMetrics(decision, s)
-}
-
-func DecisionExpired(k string, d interface{}) {
-	decision, ok := d.(*TrackedDecision)
-	if !ok {
-		log.FromContext(context.Background()).Error(fmt.Errorf("failed to cast cached item to Decision for key"), "key", k)
-		DecisionTrackerErrors.Inc(map[string]string{})
-		return
-	}
-	log.FromContext(context.Background()).Info("A decision expired", "decision", decision)
-	DecisionTrackerCacheExpirations.Inc(map[string]string{})
+	t.EmitMetrics(&decision, s)
 }
 
 func (t *Tracker) EmitMetrics(start *TrackedDecision, end *ClusterStateSnapshot) {
@@ -210,44 +172,45 @@ func (t *Tracker) EmitMetrics(start *TrackedDecision, end *ClusterStateSnapshot)
 	var clusterCostRatio, cpuRequestsRatio, memoryRequestsRatio, nodeCountRatio, desiredPodCountRatio float64
 
 	// Cluster cost change ratio
-	if end.clusterCost != 0 {
-		clusterCostRatio = start.clusterState.clusterCost / end.clusterCost
+	if end.ClusterCost != 0 {
+		clusterCostRatio = start.clusterState.ClusterCost / end.ClusterCost
 	}
 
 	// CPU requests change ratio
-	startCPU := start.clusterState.podResources[corev1.ResourceCPU]
-	endCPU := end.podResources[corev1.ResourceCPU]
+	startCPU := start.clusterState.PodResources[corev1.ResourceCPU]
+	endCPU := end.PodResources[corev1.ResourceCPU]
 	if !endCPU.IsZero() {
 		cpuRequestsRatio = float64(startCPU.MilliValue()) / float64(endCPU.MilliValue())
 	}
 
 	// Memory requests change ratio
-	startMemory := start.clusterState.podResources[corev1.ResourceMemory]
-	endMemory := end.podResources[corev1.ResourceMemory]
+	startMemory := start.clusterState.PodResources[corev1.ResourceMemory]
+	endMemory := end.PodResources[corev1.ResourceMemory]
 	if !endMemory.IsZero() {
 		memoryRequestsRatio = float64(startMemory.Value()) / float64(endMemory.Value())
 	}
 
 	// Node count change ratio
-	if end.totalNodes != 0 {
-		nodeCountRatio = float64(start.clusterState.totalNodes) / float64(end.totalNodes)
+	if end.TotalNodes != 0 {
+		nodeCountRatio = float64(start.clusterState.TotalNodes) / float64(end.TotalNodes)
 	}
 
 	// Desired pod count change ratio
-	if end.totalDesiredPodCount != 0 && end.totalDesiredPodCount != -1 {
-		desiredPodCountRatio = float64(start.clusterState.totalDesiredPodCount) / float64(end.totalDesiredPodCount)
+	if end.TotalDesiredPodCount != 0 && end.TotalDesiredPodCount != -1 {
+		desiredPodCountRatio = float64(start.clusterState.TotalDesiredPodCount) / float64(end.TotalDesiredPodCount)
 	}
 
 	// Build labels map
 	labels := map[string]string{
-		ClusterCostLabel:              convertIntoBucket(start.clusterState.clusterCost, t.bucketsThresholdMap[ClusterCostLabel]),
-		TotalCPURequestsLabel:         convertIntoBucket(float64(startCPU.MilliValue()), t.bucketsThresholdMap[TotalCPURequestsLabel]),
-		TotalMemoryRequestsLabel:      convertIntoBucket(float64(startMemory.Value()), t.bucketsThresholdMap[TotalMemoryRequestsLabel]),
-		TotalNodeCountLabel:           convertIntoBucket(float64(start.clusterState.totalNodes), t.bucketsThresholdMap[TotalNodeCountLabel]),
-		TotalDesiredPodCountLabel:     convertIntoBucket(float64(start.clusterState.totalDesiredPodCount), t.bucketsThresholdMap[TotalDesiredPodCountLabel]),
+		ClusterCostLabel:              ConvertIntoBucket(start.clusterState.ClusterCost, t.bucketsThresholdMap[ClusterCostLabel]),
+		TotalCPURequestsLabel:         ConvertIntoBucket(float64(startCPU.MilliValue()), t.bucketsThresholdMap[TotalCPURequestsLabel]),
+		TotalMemoryRequestsLabel:      ConvertIntoBucket(float64(startMemory.Value()), t.bucketsThresholdMap[TotalMemoryRequestsLabel]),
+		TotalNodeCountLabel:           ConvertIntoBucket(float64(start.clusterState.TotalNodes), t.bucketsThresholdMap[TotalNodeCountLabel]),
+		TotalDesiredPodCountLabel:     ConvertIntoBucket(float64(start.clusterState.TotalDesiredPodCount), t.bucketsThresholdMap[TotalDesiredPodCountLabel]),
 		ConsolidationTypeLabel:        start.consolidationType,
-		PodCPURequestChangeRatioLabel: convertIntoBucket(cpuRequestsRatio, t.bucketsThresholdMap[PodCPURequestChangeRatioLabel]),
-		PodMemRequestChangeRatioLabel: convertIntoBucket(memoryRequestsRatio, t.bucketsThresholdMap[PodMemRequestChangeRatioLabel]),
+		NodePoolNameLabel:             start.nodepoolName,
+		PodCPURequestChangeRatioLabel: ConvertIntoBucket(cpuRequestsRatio, t.bucketsThresholdMap[PodCPURequestChangeRatioLabel]),
+		PodMemRequestChangeRatioLabel: ConvertIntoBucket(memoryRequestsRatio, t.bucketsThresholdMap[PodMemRequestChangeRatioLabel]),
 	}
 
 	// Emit all the change ratio metrics
@@ -258,11 +221,11 @@ func (t *Tracker) EmitMetrics(start *TrackedDecision, end *ClusterStateSnapshot)
 	DesiredPodCountChangeRatio.Set(desiredPodCountRatio, labels)
 }
 
-func convertIntoBucket(metric float64, st *BucketThresholds) string {
-	for _, t := range st.thresholds {
-		if metric < t.lowerBound {
-			return t.size
+func ConvertIntoBucket(metric float64, st *BucketThresholds) string {
+	for _, t := range st.Thresholds {
+		if metric < t.LowerBound {
+			return t.Size
 		}
 	}
-	return st.biggestName
+	return st.BiggestName
 }
