c762e215d15e553a8f35ce5d3050a6b58ac46ae9
ryan-mist <ryanmist@amazon.com>
2026-01-14 16:10:56 -0800

    fix

--- pkg/controllers/provisioning/provisioner.go
+++ pkg/controllers/provisioning/provisioner.go
@@ -233,7 +233,7 @@ func (p *Provisioner) consolidationWarnings(ctx context.Context, pods []*corev1.
 	}
 }
 
-var ErrNodePoolsNotFound = errors.New("no dynamic nodepools found")
+var ErrNodePoolsNotFound = errors.New("no nodepools found")
 
 //nolint:gocyclo
 func (p *Provisioner) NewScheduler(
@@ -357,9 +357,9 @@ func (p *Provisioner) Schedule(ctx context.Context) (scheduler.Results, error) {
 	)
 	if err != nil {
 		if errors.Is(err, ErrNodePoolsNotFound) {
-			log.FromContext(ctx).Info("no nodepools found")
+			log.FromContext(ctx).Info("no dyanmic nodepools found")
 			p.cluster.MarkPodSchedulingDecisions(ctx, lo.SliceToMap(pods, func(p *corev1.Pod) (*corev1.Pod, error) {
-				return p, fmt.Errorf("no nodepools found")
+				return p, fmt.Errorf("no dyanmic nodepools found")
 			}), nil, nil)
 			return scheduler.Results{}, nil
 		}
