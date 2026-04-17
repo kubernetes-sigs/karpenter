ee05017e980c811725b8ea4e62da31d45d136e72
onyx <jonesflp@amazon.com>
2026-04-17 11:03:24 -0700

    test: Add unit tests for pod deletion cost controller (kp-fy3)
    Add comprehensive unit tests covering ranking, annotation management,
    change detection, and controller reconciliation for the pod deletion
    cost feature. Tests validate two-tier partitioning, sentinel annotation
    detection, customer-managed annotation preservation, and change
    detection optimization.
    
    23 tests across 4 test files:
    - ranking_test.go: tier partitioning and rank assignment
    - annotation_test.go: sentinel detection and pod annotation updates
    - changedetector_test.go: hash-based change detection
    - controller_test.go: reconciliation and feature gate behavior

---
+++ pkg/controllers/pod/deletioncost/suite_test.go
@@ -0,0 +1,84 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package deletioncost_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	clock "k8s.io/utils/clock/testing"
+
+	coreapis "sigs.k8s.io/karpenter/pkg/apis"
+	"sigs.k8s.io/karpenter/pkg/cloudprovider/fake"
+	"sigs.k8s.io/karpenter/pkg/controllers/state"
+	"sigs.k8s.io/karpenter/pkg/controllers/state/informer"
+	"sigs.k8s.io/karpenter/pkg/operator/options"
+	"sigs.k8s.io/karpenter/pkg/state/cost"
+	"sigs.k8s.io/karpenter/pkg/test"
+	. "sigs.k8s.io/karpenter/pkg/test/expectations"
+	"sigs.k8s.io/karpenter/pkg/test/v1alpha1"
+	. "sigs.k8s.io/karpenter/pkg/utils/testing"
+)
+
+var ctx context.Context
+var env *test.Environment
+var cluster *state.Cluster
+var cloudProvider *fake.CloudProvider
+var fakeClock *clock.FakeClock
+var nodeStateController *informer.NodeController
+var nodeClaimStateController *informer.NodeClaimController
+var recorder *test.EventRecorder
+
+func TestAPIs(t *testing.T) {
+	ctx = TestContextWithLogger(t)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "DeletionCost")
+}
+
+var _ = BeforeSuite(func() {
+	env = test.NewEnvironment(test.WithCRDs(coreapis.CRDs...), test.WithCRDs(v1alpha1.CRDs...))
+	opts := test.Options()
+	opts.FeatureGates.PodDeletionCostManagement = true
+	opts.PodDeletionCostRankingStrategy = "Random"
+	opts.PodDeletionCostChangeDetection = true
+	ctx = options.ToContext(ctx, opts)
+	cloudProvider = fake.NewCloudProvider()
+	fakeClock = clock.NewFakeClock(time.Now())
+	cluster = state.NewCluster(fakeClock, env.Client, cloudProvider)
+	nodeStateController = informer.NewNodeController(env.Client, cluster)
+	clusterCost := cost.NewClusterCost(ctx, cloudProvider, env.Client)
+	nodeClaimStateController = informer.NewNodeClaimController(env.Client, cloudProvider, cluster, clusterCost)
+	recorder = test.NewEventRecorder()
+})
+
+var _ = AfterSuite(func() {
+	Expect(env.Stop()).To(Succeed(), "Failed to stop environment")
+})
+
+var _ = BeforeEach(func() {
+	cloudProvider.Reset()
+	cloudProvider.InstanceTypes = fake.InstanceTypesAssorted()
+	recorder.Reset()
+})
+
+var _ = AfterEach(func() {
+	ExpectCleanedUp(ctx, env.Client)
+	cluster.Reset()
+})
